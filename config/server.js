2df92cc82ba1ee1473f7543d3532fc1c83bc7c11
Charlie963 <rubancharles99@gmail.com>
2023-06-07 19:28:28 +0530

    d

--- config/server.js
+++ config/server.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '192.168.0.105'),
+  host: env('HOST', '127.0.0.3'),
   port: env.int('PORT', 1337),
   app: {
     keys: ['6Cnpm6gYtd6sh321OiVG6A==','MKVnr3VPt1TZZjq5EcB3rQ==','vLRkv5MpSC1YADRs/REQyQ==','kcNkFP0XztSWOLFD04USxg=='],
