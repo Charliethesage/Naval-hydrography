fec49521646b63879c3eaedc80708364a25d7aec
Charlie963 <rubancharles99@gmail.com>
2023-06-07 14:51:52 +0530

    fdf

--- config/server.js
+++ config/server.js
@@ -2,6 +2,6 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS')
+    keys: ['6Cnpm6gYtd6sh321OiVG6A==','MKVnr3VPt1TZZjq5EcB3rQ==','vLRkv5MpSC1YADRs/REQyQ==','kcNkFP0XztSWOLFD04USxg=='],
   },
 });
