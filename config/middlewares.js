213fe82ae73bfd0be0a35a0da0dbc7f30a6679e4
Charlie963 <rubancharles99@gmail.com>
2023-06-11 23:51:24 +0530

    fdd

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,25 @@
 module.exports = [
   "strapi::errors",
-  "strapi::security",
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   "strapi::cors",
   "strapi::poweredBy",
   "strapi::logger",
